596a15f92926f81b0c709364057bdc619eed8974
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-28 18:09:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function volume_sphere(event) {
+	
+   event.preventDefault(); // prevent form submission
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+	
+    let radius = document.querySelector("#radius");
+    let volume = document.querySelector("#volume");
+    let R = parseFloat(radius.value);
+
+    // Check if the input is a non-negative number
+    if (R < 0) {
+        volume.value = 'NaN';
+    } else {
+        let V = (4/3)*Math.PI*Math.pow(R, 3);
+        volume.value = V.toFixed(4); // Round to 4 decimal places
+    }
+}
+
+window.onload = function() {
+    document.getElementById('MyForm').onsubmit = volume_sphere;
+}
